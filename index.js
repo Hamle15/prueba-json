f911be5d87ea8765756eb56b39d781bb651a17e1
Hamle15 <hamletcruzpirazan@gmail.com>
2024-10-18 11:15:39 -0500

    xd

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-// Ruta básica de prueba
-app.get("/", (req, res) => {
-  res.send({
-    lista: ["hola", "hola", "hola"],
-  });
-});
-
-// Inicia el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+{
+    "lista": ["hola", "Hola", "hoa"]
+}
